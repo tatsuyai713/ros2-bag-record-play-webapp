5b65fb3646fdcab0fd555b43f393f3887f0fa6ab
Tatsuya Ishikawa <tatsuyai713@gmail.com>
2025-01-17 02:22:16 +0900

    Change port

--- ros2_bag_player/app.py
+++ ros2_bag_player/app.py
@@ -185,4 +185,4 @@ def play_output():
     return jsonify({"logs": formatted})
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0', port=5001)
